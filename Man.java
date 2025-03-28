66200730cba49b5e455c4c2294cfea55e3dd65a0
Shamoto <svkrohmal@bk.ru>
2025-03-28 12:16:15 +0300

    test do man.java

---
+++ Man.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Man {
+    public static void main(String[] args) {
+        
+    }
+    public Man(){
+
+    }
+    public Man(String name, int age) {
+        this.name = name;
+    }
+}
